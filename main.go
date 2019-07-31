2fb3e852dffca027f97856122f07dcc83a408b31
Mark Bradley <braddle@gmail.com>
2019-07-31 11:40:59 +0100

    Introducing Content Negotiation for v1
    Creating a ResponseProcessor to handle v1 get requests and making v1 the default response when the accept header is just application/json

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/jchannon/negotiator"
@@ -37,7 +39,35 @@ func personHandler(w http.ResponseWriter, r *http.Request) {
 		HasTattoo: false,
 	}
 
-	if err := negotiator.Negotiate(w, r, p); err != nil {
+	n := negotiator.New(&personV1Processor{})
+	if err := n.Negotiate(w, r, p); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+type personV1Processor struct{}
+
+func (p *personV1Processor) CanProcess(mediaRange string) bool {
+	return strings.EqualFold(mediaRange, "application/vnd.person.v1+json") ||
+		strings.EqualFold(mediaRange, "application/json")
+}
+
+func (p *personV1Processor) Process(w http.ResponseWriter, req *http.Request, dataModel interface{}, context ...interface{}) error {
+	w.Header().Set("Content-Type", "application/vnd.person.v1+json")
+
+	person, _ := dataModel.(person)
+
+	p1 := map[string]interface{}{
+		"id":         person.Id,
+		"first_name": person.FirstName,
+		"last_name":  person.LastName,
+		"age":        person.Age,
+		"has_tattoo": person.HasTattoo,
+	}
+
+	j, _ := json.Marshal(p1)
+
+	w.Write(j)
+
+	return nil
 }
